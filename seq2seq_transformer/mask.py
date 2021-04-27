434b5fad3c160497197269e94b4faae550a23d7c
yanghh0 <13588036019@163.com>
2021-04-27 16:43:18 +0800

    Update mask.py

--- seq2seq_transformer/mask.py
+++ seq2seq_transformer/mask.py
@@ -22,3 +22,18 @@ def get_subsequent_mask(targets):
     b, t = targets.size()
     subsequent_mask = (1 - torch.triu(torch.ones((1, t, t), device=targets.device), diagonal=1)).bool()
     return subsequent_mask
+
+
+if __name__ == "__main__":
+    src_seqs = torch.LongTensor([[1, 2, 3, 4],
+                                 [4, 3, 2, 0],
+                                 [7, 4, 0, 0]])
+    trg_seqs = torch.LongTensor([[3, 1, 3, 4],
+                                 [4, 6, 2, 0],
+                                 [7, 7, 0, 0]])
+
+    pad_mask = get_pad_mask(src_seqs, pad_idx=0)
+    subsequent_mask = get_subsequent_mask(trg_seqs)
+
+    print(pad_mask)
+    print(subsequent_mask)
