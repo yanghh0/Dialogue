56eba406ac5e6fa69825507da520f59599e739d6
yanghh0 <13588036019@163.com>
2021-04-27 11:15:03 +0800

    Create feed_forward.py

---
+++ seq2seq_transformer/feed_forward.py
@@ -0,0 +1,43 @@
+
+import sys
+sys.path.append(".")
+sys.path.append("..")
+
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class PositionwiseFeedForward(nn.Module):
+    """A two-feed-forward-layer module
+    """
+
+    def __init__(self, d_in, d_hid, dropout=0.1):
+        super().__init__()
+        self.w_1 = nn.Linear(d_in, d_hid) # position-wise
+        self.w_2 = nn.Linear(d_hid, d_in) # position-wise
+        self.layer_norm = nn.LayerNorm(d_in, eps=1e-6)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x):
+        residual = x
+        x = self.w_1(x)
+        x = F.relu(x)
+        x = self.w_2(x)
+        x = self.dropout(x)
+        x += residual
+        x = self.layer_norm(x)
+
+        return x
+
+
+if __name__ == "__main__":
+    # (b, t, h)
+    x = torch.randn(3, 8, 10)
+    use_gpu = True
+    net = PositionwiseFeedForward(d_in=10, d_hid=20)
+    if use_gpu:
+        net.cuda()
+        x = x.cuda()
+    out = net(x)
