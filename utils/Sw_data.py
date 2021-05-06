b83e0da4d4dc8114da8b12d87e1a1a397b2b38ff
yanghh0 <13588036019@163.com>
2021-05-06 23:20:30 +0800

    Update Sw_data.py

--- utils/Sw_data.py
+++ utils/Sw_data.py
@@ -25,35 +25,35 @@ class Data:
 
         print("Done loading corpus!")
 
-    def process(self, data):
-        new_dialog = []
-        new_meta = []
-        new_utts = []
+    def process(self, calls):
+        calls_dialogs = []
+        calls_metas = []
+        calls_utts = []
         all_lengths = []
 
-        for line in data:
+        for one_call in calls:
             lower_utts = [(caller, normalizeStringNLTK(utt).split(" "), feat)
-                          for caller, utt, feat in line["utts"]]
+                          for caller, utt, feat in one_call["utts"]]
             all_lengths.extend([len(u) for c, u, f in lower_utts])
 
-            a_age = float(line["A"]["age"]) / 100.0
-            b_age = float(line["B"]["age"]) / 100.0
-            a_edu = float(line["A"]["education"]) / 3.0
-            b_edu = float(line["B"]["education"]) / 3.0
-            vec_a_meta = [a_age, a_edu] + ([0, 1] if line["A"]["sex"] == "FEMALE" else [1, 0])
-            vec_b_meta = [b_age, b_edu] + ([0, 1] if line["B"]["sex"] == "FEMALE" else [1, 0])
+            a_age = float(one_call["A"]["age"]) / 100.0
+            b_age = float(one_call["B"]["age"]) / 100.0
+            a_edu = float(one_call["A"]["education"]) / 3.0
+            b_edu = float(one_call["B"]["education"]) / 3.0
+            vec_a_meta = [a_age, a_edu] + ([0, 1] if one_call["A"]["sex"] == "FEMALE" else [1, 0])
+            vec_b_meta = [b_age, b_edu] + ([0, 1] if one_call["B"]["sex"] == "FEMALE" else [1, 0])
 
             # eg. ([0.5, 0.6666666666666666, 1, 0], [0.75, 0.6666666666666666, 1, 0], '"BUYING A CAR"')
-            meta = (vec_a_meta, vec_b_meta, line["topic"])
+            meta = (vec_a_meta, vec_b_meta, one_call["topic"])
             # 1 is own utt and 0 is other's utt
             dialog = [(utt, int(caller=="B"), feat) for caller, utt, feat in lower_utts]
 
-            new_utts.extend([utt for caller, utt, feat in lower_utts])
-            new_meta.append(meta)
-            new_dialog.append(dialog)
+            calls_utts.extend([utt for caller, utt, feat in lower_utts])
+            calls_metas.append(meta)
+            calls_dialogs.append(dialog)
 
         print("Max utt len %d, mean utt len %.2f" % (np.max(all_lengths), float(np.mean(all_lengths))))
-        return new_dialog, new_meta, new_utts
+        return calls_dialogs, calls_metas, calls_utts
 
     def build_alphabet(self):
         # create word alphabet
@@ -109,8 +109,28 @@ class Data:
         id_test = _to_id_corpus(self.test_corpus[0])
         return {'train': id_train, 'valid': id_valid, 'test': id_test}
 
-    def data_generator(self, batch_size):
-        pass
+    def data_generator(self, batch_size, data_type, backward_size, step_size):
+        dialogs = self.dialog_corpus[data_type]
+        metas = self.meta_corpus[data_type]
+        lengths = [len(dialog) for dialog in dialogs]
+        indexes = list(np.argsort(lengths))
+        total_calls = len(dialogs)
+
+        ptr = 0
+
+        temp_num_batch = total_calls // batch_size
+        batch_indexes = []
+        for i in range(temp_num_batch):
+            batch_indexes.append(indexes[i*batch_size : (i+1)*batch_size])
+
+        left_calls = total_calls - temp_num_batch * batch_size
+        np.random.shuffle(batch_indexes)
+
+        grid_indexes = []
+        for idx, b_ids in enumerate(batch_indexes):
+            batch_lengths = [lengths[i] for i in b_ids]
+            max_length = lengths[b_ids[-1]]
+            min_length = lengths[b_ids[0]]
 
 
 if __name__ == "__main__":
@@ -118,3 +138,4 @@ if __name__ == "__main__":
     corpus_file = os.path.join("..", "datasets", corpus_name, "full_swda_clean_42da_sentiment_dialog_corpus.p")
 
     obj = Data(corpus_file)
+    obj.data_generator(batch_size=128, data_type="train")
