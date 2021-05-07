b54dbbf1dbb2e61f3ae2518cee621213b0cccdbd
yanghh0 <13588036019@163.com>
2021-05-07 22:45:17 +0800

    Create train.py

---
+++ cvae/train.py
@@ -0,0 +1,17 @@
+
+import sys
+sys.path.append(".")
+sys.path.append("..")
+
+
+import torch
+import torch.nn as nn
+from cvae.config import Config
+
+
+class QATrainer:
+	pass
+
+
+if __name__ == "__main__":
+	pass
