a1454850c2cc83308ee3533619242ae82c2bda6c
yanghh0 <13588036019@163.com>
2021-05-07 22:31:48 +0800

    Update config.py

--- cvae/config.py
+++ cvae/config.py
@@ -11,3 +11,8 @@ class Config():
     step_size = 1             # internal usage
     batch_size = 60           # mini-batch size
     backward_size = 10        # how many utterance kept in the context window
+    ctx_hidden_size = 600     # context encoder hidden size
+    utt_hidden_size = 300     # utterance encoder hidden size
+    res_hidden_size = 400     # response decoder hidden size
+    latent_size = 200         # the dimension of latent variable
+    num_layer = 1             # number of context RNN layers
