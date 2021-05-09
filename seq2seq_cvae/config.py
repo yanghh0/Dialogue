4534077cd4341c7dfca24c2c2bf815e387b7d22a
yanghh0 <13588036019@163.com>
2021-05-09 14:15:51 +0800

    Update config.py

--- seq2seq_cvae/config.py
+++ seq2seq_cvae/config.py
@@ -2,26 +2,46 @@
 import torch
 
 
-class Config():
-    max_utt_length = 40           # max number of words in an utterance
-    word_embed_size = 200         # word embedding size
-    topic_embed_size = 30         # topic embedding size
-    act_embed_size = 30           # dialog act embedding size
-    use_hcf = True                # use dialog act in training (if turn off kgCVAE -> CVAE)
-    step_size = 1                 # internal usage
-    batch_size = 60               # mini-batch size
-    backward_size = 10            # how many utterance kept in the context window
-    ctx_hidden_size = 600         # context encoder hidden size
-    utt_hidden_size = 300         # utterance encoder hidden size
-    res_hidden_size = 400         # response decoder hidden size
-    latent_size = 200             # the dimension of latent variable
-    num_layer = 1                 # number of context RNN layers
-    init_lr = 0.001               # initial learning rate
-    keep_prob = 1.0               # drop out rate
+class Config:
+    max_utt_length = 40
 
-    """
-    bow: add word embedding together
-    rnn: RNN utterance encoder
-    bi_rnn: bi_directional RNN utterance encoder
-    """
-    utt_encode_type = "bi_rnn"    # how to encode utterance.
+    # use dialog act in training (if turn off kgCVAE -> CVAE)
+    use_hcf = True
+    use_gpu = True
+
+    # It needs to be set externally. The current value is set arbitrarily.
+    word_vocab_size = 1
+    topic_vocab_size = 1
+    act_vocab_size = 1
+
+    word_embed_size = 200
+    topic_embed_size = 30
+    act_embed_size = 30
+
+    utt_num_layer = 1
+    utt_input_size = word_embed_size
+    utt_hidden_size = 300
+
+    ctx_num_layer = 1
+    ctx_input_size = utt_hidden_size * 2 + 2
+    ctx_hidden_size = 600
+
+    prior_input_size = topic_embed_size + 4 + 4 + ctx_hidden_size
+    recog_input_size = prior_input_size + utt_hidden_size * 2
+    if use_hcf:
+        recog_input_size += 30
+    latent_size = 200
+
+    gen_inputs_size = prior_input_size + latent_size
+
+    dec_inputs_size = gen_inputs_size
+    if use_hcf:
+        dec_inputs_size += 30
+    res_hidden_size = 400
+
+    step_size = 1                                  # internal usage
+    batch_size = 60                                # mini-batch size
+    backward_size = 10                             # how many utterance kept in the context window
+
+    init_lr = 0.001                                # initial learning rate
+    keep_prob = 1.0                                # drop out rate
