9a0ad93ab287605e10b07db06e48805880e72442
yanghh0 <13588036019@163.com>
2021-05-09 15:32:19 +0800

    Update stack.py

--- test/stack.py
+++ test/stack.py
@@ -9,4 +9,7 @@ T2 = torch.tensor([[10, 20, 30],
                    [40, 50, 60],
                    [70, 80, 90]])
 
+print(torch.stack((T1, T2)))
+print(torch.stack((T1, T2)).shape)
 print(torch.stack((T1, T2), dim=1))
+print(torch.stack((T1, T2), dim=1).shape)
