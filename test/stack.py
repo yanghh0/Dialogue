6555c9a270c40dcf26e624e2187b5a540dc187cc
yanghh0 <13588036019@163.com>
2021-05-09 15:30:35 +0800

    Create stack.py

---
+++ test/stack.py
@@ -0,0 +1,12 @@
+
+import torch
+
+
+T1 = torch.tensor([[1, 2, 3],
+                   [4, 5, 6],
+                   [7, 8, 9]])
+T2 = torch.tensor([[10, 20, 30],
+                   [40, 50, 60],
+                   [70, 80, 90]])
+
+print(torch.stack((T1, T2), dim=1))
