e0249ca808360ab524ab433134a3e3aac2fa0cbf
yanghh0 <13588036019@163.com>
2021-05-10 22:37:58 +0800

    Update decoder.py

--- seq2seq_cvae/decoder.py
+++ seq2seq_cvae/decoder.py
@@ -12,6 +12,13 @@ from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence
 from seq2seq_cvae.config import Config
 
 
+# Default tokens
+PAD_token = 0  # Used for padding short sentences
+SOS_token = 1  # Start-of-sentence token
+EOS_token = 2  # End-of-sentence token
+UNK_token = 3  # Unkonw token
+
+
 class DecoderRNN(nn.Module):
     def __init__(self):
         super(DecoderRNN, self).__init__()
@@ -42,16 +49,7 @@ class DecoderRNN(nn.Module):
                 )
         return cell
 
-    def forward(self, inputs, enc_outputs, inputs_length, attr_vector):
-        """shape of inputs: (60, 40, 200)
-           shape of attr_vector: (60, 30)
-        """
-        if Config.dec_num_layer > 1:
-            dec_init_state = [self.dec_init_state_net[i](enc_outputs) for i in range(Config.dec_num_layer)]
-            dec_init_state = torch.stack(dec_init_state)
-        else:
-            dec_init_state = self.dec_init_state_net(enc_outputs).unsqueeze(0)
-
+    def train_loop(self, inputs, dec_init_state, inputs_length, attr_vector):
         inputs = F.dropout(inputs, 1 - Config.keep_prob, self.training)
         if attr_vector is not None:
             inputs = torch.cat([inputs, attr_vector.unsqueeze(1).expand(inputs.size(0), inputs.size(1), attr_vector.size(1))], 2)
@@ -92,6 +90,59 @@ class DecoderRNN(nn.Module):
         outputs = self.fc(outputs)
 
         return outputs, state, None
+
+    def inference_loop(self, dec_init_state, attr_vector, word_embedding):
+        batch_size = dec_init_state.size(1)
+        cell_state, cell_input, cell_output = dec_init_state, None, None
+
+        outputs = []
+        context_state = []
+
+        for time in range(Config.max_utt_length + 1):
+            if cell_output is None:
+                next_input_id = dec_init_state.new_full((batch_size,), SOS_token, dtype=torch.long)
+                done = dec_init_state.new_zeros(batch_size, dtype=torch.uint8)
+                cell_state = dec_init_state
+            else:
+                cell_output = self.fc(cell_output)
+                outputs.append(cell_output)
+
+                next_input_id = torch.max(cell_output, 1)[1]
+                next_input_id = next_input_id * (~done).long() # make sure the next_input_id to be 0 if done
+                done = (next_input_id == EOS_token) | done
+                context_state.append(next_input_id)
+
+            next_input = word_embedding(next_input_id)
+            if attr_vector is not None:
+                next_input = torch.cat([next_input, attr_vector], 1)
+            if done.long().sum() == batch_size:
+                break
+
+            cell_output, cell_state = self.decoder(next_input.unsqueeze(1), cell_state)
+            # Squeeze the time dimension
+            cell_output = cell_output.squeeze(1)
+
+            # zero out done sequences
+            cell_output = cell_output * (~done).float().unsqueeze(1)
+
+        return torch.cat([_.unsqueeze(1) for _ in outputs], 1), \
+               cell_state, \
+               torch.cat([_.unsqueeze(1) for _ in context_state], 1)
+
+    def forward(self, mode, inputs, enc_outputs, inputs_length, attr_vector, word_embedding):
+        """shape of inputs: (60, 40, 200)
+           shape of attr_vector: (60, 30)
+        """
+        if Config.dec_num_layer > 1:
+            dec_init_state = [self.dec_init_state_net[i](enc_outputs) for i in range(Config.dec_num_layer)]
+            dec_init_state = torch.stack(dec_init_state)
+        else:
+            dec_init_state = self.dec_init_state_net(enc_outputs).unsqueeze(0)
+
+        if mode == "test":
+            return self.inference_loop(dec_init_state, attr_vector, word_embedding)
+        else:
+            return self.train_loop(inputs, dec_init_state, inputs_length, attr_vector)
 
 
 if __name__ == "__main__":
